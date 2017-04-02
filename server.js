e19a94a25128540a7581fbefe97724528c8889b8
dowdje <dowdje89@gmail.com>
2017-04-02 17:06:09 -0400

    add gitignore

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express'),
     hosts = require('./hosts'),
-    path = require("path");
+    path = require("path"),
+    favicon = require('serve-favicon');
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(favicon(path.join(__dirname, '/public/images/favicon.ico')))
 
 app.get('/',function(req, res){
   res.sendFile(path.join(__dirname+'/index.html'));
