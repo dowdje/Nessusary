5c7859337234b716bc388fd9d0af2c07932f37cf
dowdje <dowdje89@gmail.com>
2017-04-02 14:53:58 -0400

    add node server routing; configure public directory

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express'),
-    hosts = require('./hosts');
+    hosts = require('./hosts'),
+    path = require("path");
 
 var app = express();
 
-app.get('/download/request', hosts.allHosts);
+app.use(express.static(__dirname + '/public'));
 
+
+app.get('/',function(req, res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/download/request', hosts.allHosts);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
